8148485bb494ba01574368d7c9bdbaec496bcd5d
이진하 <dkwlsfk2323@gmail.com>
2020-03-31 14:12:59 +0900

    tableview cell 액션 추가

--- twitterClone/ViewController.swift
+++ twitterClone/ViewController.swift
@@ -20,18 +20,24 @@ struct Tweet{
 }
 
 class ViewController: UIViewController {
+
+    private var refreshControl = UIRefreshControl()
     
+    @IBOutlet weak var deleteButton: UIBarButtonItem!
     @IBOutlet weak var TweetTable: UITableView!
-    let tweetList: [Tweet] = [
+    var tweetList: [Tweet] = [
         Tweet(Id: "농담곰", content: "ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ", profile: UIImage(named: "농담곰")),
         Tweet(Id: "무민", content: "안녕하세요 무민 입니다", profile: UIImage(named: "무민")),
         Tweet(Id: "커비", content: "안녕하세요 커비 입니다", profile: UIImage(named: "커비")),
         Tweet(Id: "스누피", content: "ddd", profile: UIImage(named: "스누피")),
-        Tweet(Id: "e", content: "eee", profile: UIImage(named: "")),
-        Tweet(Id: "f", content: "fff", profile: UIImage(named: "")),
-        Tweet(Id: "g", content: "ggg", profile: UIImage(named: ""))
+        Tweet(Id: "스펀지밥", content: "eee", profile: UIImage(named: "스펀지밥")),
+        Tweet(Id: "뚱이", content: "ㅎㅎㅎㅎ", profile: UIImage(named: "뚱이")),
+        Tweet(Id: "하리보", content: "ggg", profile: UIImage(named: "하리보"))
     
     ]
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -44,18 +50,50 @@ class ViewController: UIViewController {
         //위임
         self.TweetTable.rowHeight = UITableView.automaticDimension
         self.TweetTable.estimatedRowHeight = 660.0
+        self.TweetTable.isEditing = true
+
     }
-    
 
 }
 
 
 extension ViewController: UITableViewDelegate,UITableViewDataSource {
     
+    //왼쪽으로 스와이프할 때 액션
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+          self.tweetList.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+   }
     
+    //왼쪽으로 스와이프할 떄 액션
+    func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let share = UIContextualAction(style: .normal, title: "공유"){action,view,completion in completion(true)
+        }
+        return UISwipeActionsConfiguration(actions: [share])
+    }
+    //cell 이동
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+
+        let Tweet = self.tweetList[sourceIndexPath.row]
+        self.tweetList.remove(at: sourceIndexPath.row)
+        self.tweetList.insert(Tweet, at: destinationIndexPath.row)
+    }
+    //delete 버튼 안보이게 함
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .none
+    }
+
+    func tableView(_ tableView: UITableView, shouldIndentWhileEditingRowAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    //cell개수
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.tweetList.count
     }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "TweetCell", for: indexPath) as? TweetCell else { return UITableViewCell() }
         //identifier가 "TweetCell"이란 cell을 재사용, cell에서 등록한 id,content,profile을 현재 tableViewlist에 있는것들을 각각 연결시켜줘야 함
@@ -67,5 +105,6 @@ extension ViewController: UITableViewDelegate,UITableViewDataSource {
         cell.Id.font = .boldSystemFont(ofSize: 16)
         return cell
     }
+
     
 }
